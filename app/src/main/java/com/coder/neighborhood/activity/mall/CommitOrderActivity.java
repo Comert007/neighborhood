8b8e9d290afdc3479375c12b7a2b3945718a8370
sourfeng <sourfeng@163.com>
2018-03-07 15:34:09 +0800

    add the mall ui relative content

---
+++ app/src/main/java/com/coder/neighborhood/activity/mall/CommitOrderActivity.java
@@ -0,0 +1,22 @@
+package com.coder.neighborhood.activity.mall;
+
+import com.coder.neighborhood.activity.BaseActivity;
+import com.coder.neighborhood.mvp.model.VoidModel;
+import com.coder.neighborhood.mvp.vu.VoidView;
+
+/**
+ * @author feng
+ * @Date 2018/3/7.
+ */
+
+public class CommitOrderActivity extends BaseActivity<VoidView,VoidModel> {
+    @Override
+    protected int getLayoutResId() {
+        return 0;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}
